5f5d3d09ecd475eb9fc0328395297fcd0fcf498d
Nick <nicholas.kuan@gmail.com>
2015-02-24 18:53:34 +1300

    Change color

--- LabMayhem/Game1.cs
+++ LabMayhem/Game1.cs
@@ -73,7 +73,7 @@ namespace LabMayhem
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.PaleGreen);
 
             // TODO: Add your drawing code here
 
